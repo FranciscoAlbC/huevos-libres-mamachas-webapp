0cd79abf4c9ade241b7ea52a20261b2bd0acecdf
Francisco Alberca <francisco.albercac@gmail.com>
2024-12-15 20:26:10 -0500

    First commit: Spring Boot poroject setup

---
+++ src/main/java/pe/com/mamachas/HuevosLibresMamachasWebappApplication.java
@@ -0,0 +1,13 @@
+package pe.com.mamachas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuevosLibresMamachasWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuevosLibresMamachasWebappApplication.class, args);
+	}
+
+}
