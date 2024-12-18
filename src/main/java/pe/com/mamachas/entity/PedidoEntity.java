324e48123fbd732a46351c956655f87f830cea3d
SugeyStudy <130309756+SugeyStudy123@users.noreply.github.com>
2024-12-18 12:22:32 -0500

    Add files via upload

---
+++ src/main/java/pe/com/mamachas/entity/PedidoEntity.java
@@ -0,0 +1,39 @@
+package pe.com.lasmamachas.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Entity(name = "PedidoEntity")
+@Table(name = "pedido")
+public class PedidoEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name = "codigo")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    @Column(name = "fechaPedido", nullable = true)
+    private LocalDate fechaPedido;
+
+    
+}
+ 
